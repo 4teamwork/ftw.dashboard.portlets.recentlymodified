2e6adf6b2dfc5897d69ef41a8dc308e3b861a5fb
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-05-07 16:27:13 +0200

    Back to development: 1.2.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.2.5'
+version = '1.2.6.dev0'
 maintainer = 'Philipp Gross'
 tests_require = ['zope.testing',
                  'plone.app.testing',
