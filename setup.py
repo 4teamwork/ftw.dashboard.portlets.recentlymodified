e989e241cb4385e3232e6b3e59d1034781fb3b9a
Jonas Baumann <jone@jone.ch>
2012-11-12 14:03:46 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 
 version = '1.2.7.dev0'
 maintainer = 'Philipp Gross'
+
 tests_require = ['zope.testing',
                  'plone.app.testing',
                  'plone.mocktestcase',
@@ -11,8 +12,9 @@ tests_require = ['zope.testing',
 setup(name='ftw.dashboard.portlets.recentlymodified',
       version=version,
       description="Recently modified portlet for the dashboard",
-      long_description=open("README.rst").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" + \
                        open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -21,23 +23,27 @@ setup(name='ftw.dashboard.portlets.recentlymodified',
         "Framework :: Plone :: 4.0",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules", ],
-      keywords='',
+
+      keywords='ftw dashboard portlet recentlymodified',
       author='4teamwork GmbH',
       maintainer=maintainer,
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.dashboard.portlets.recentlymodified',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.dashboard', 'ftw.dashboard.portlets'],
       include_package_data=True,
       zip_safe=False,
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
+
       install_requires=[
           'setuptools',
           'collective.js.jqsmartTruncation',
           # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
       entry_points="""
         # -*- Entry points: -*-
         [z3c.autoinclude.plugin]
