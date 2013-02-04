60e28ae7fbb86bb393d46882febbae922bbd3c63
Jonas Baumann <jone@jone.ch>
2013-02-04 10:36:36 +0100

    JavaScript: jQuery function with $ in scope.

--- ftw/dashboard/portlets/recentlymodified/recentlymodified.truncate-long-text.js
+++ ftw/dashboard/portlets/recentlymodified/recentlymodified.truncate-long-text.js
@@ -1,4 +1,4 @@
-$(function(){
+jQuery(function($){
     $(window).bind("load", function() {
         $('dl.portlet.portletRecent span.title a').smartTruncation();
         $('dl.portlet.portletRecent dd.portletItem span.title').css('visibility','visible');
